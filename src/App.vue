c9917d2c6b2c3bab22629260a44df2a42e3cce8c
ngxuantai <xuantai1902@gmail.com>
2024-04-03 14:52:35 +0700

    Tutorial #3: Create Vue project with vue-cli

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HeaderApp />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import HeaderApp from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    HeaderApp,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>This is header app</h1>
+</template>
+
+<script>
+export default {
+  name: "HeaderApp",
+  props: {},
+};
+</script>
+
+<style></style>
